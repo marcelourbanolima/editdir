d319d493b6ae3bc01aa3af278e126dd8c07429f4
Marcelo Urbano Lima <Marcelo.Urbano.Lima@Kyndryl.com>
2024-01-08 12:03:22 -0300

    calling the editor and merging entries to know what to do with them. Not taking actions yet

--- main.go
+++ main.go
@@ -6,22 +6,26 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"os/exec"
 	"sort"
 	"strconv"
 	"strings"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 // Status define the possible status of an Entry and give a hint of what action
 // should be taken on and Entry.
 // It's zero value is not used
-type Status int
+type Status string
 
 const (
-	StatusError  Status = 1
-	StatusAbort  Status = 2
-	StatusRename Status = 3
-	StatusDelete Status = 4
-	StatusIgnore Status = 5
+	StatusIgnore Status = "Ignore" // The default status
+	StatusError  Status = "Error"
+	StatusCancel Status = "Cancel"
+	StatusRename Status = "Rename"
+	StatusDelete Status = "Delete"
 )
 
 type Entry struct {
@@ -42,18 +46,83 @@ type Entry struct {
 }
 
 func (e Entry) String() string {
-	return fmt.Sprintf("%d '%s' -> '%s'", e.ID, e.Path, e.NewPath)
+	return fmt.Sprintf("%d '%s' -> '%s' Status: %v", e.ID, e.Path, e.NewPath, e.Status)
 }
 
 type Entries map[int]Entry
 
+func (es Entries) String() string {
+	keys := sort.IntSlice{}
+	for k := range es {
+		keys = append(keys, k)
+	}
+	keys.Sort()
+	sb := strings.Builder{}
+	for _, key := range keys {
+		e := es[key]
+		_, err := sb.WriteString(fmt.Sprintf("%d %s\n", e.ID, e.Path))
+		if err != nil {
+			panic(fmt.Sprintf("error building Entries' String(): %v", err))
+		}
+	}
+	return sb.String()
+}
+
+// Update updates Entries from other entries, updating their NewPath and Status
+func (es *Entries) Update(from Entries) {
+	for i, _ := range *es {
+		e := (*es)[i]
+		ee, ok := from[e.ID]
+		if !ok {
+			e.Status = StatusDelete
+			(*es)[i] = e
+			continue
+		}
+		e.NewPath = ee.Path
+		e.Status = StatusRename
+		(*es)[i] = e
+	}
+}
+
 func main() {
+	err := run()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+// TODO:
+// Load CLI options
+// Receive the path where dirs reside and store in a temporary file
+// add line numbers to each path in the temporary file
+// add to the temp file a line showing how this works
+// Run the editor on that temporary file and wait for it to finish
+// when the editor exists check its return code, if rc!= exit
+// read all lines of the temp file, if any of them have the word cancel or abort then exit
+// check if there are no lines with the same number, exit with error
+// check if there are are new files with the same name (duplicates). Warn
+
+// options to:
+// delete file if line is deleted from file
+// enable check for duplicate names in output
+// enable check for when the file already exists
+// include dot files (call it 'hidden' if I can work with hidden files in windows)
+// load files recursively, this implies: needs to know what to do with directories, what to do with directory entries, etc
+
+func run() error {
+	// Things hardcoded only for now
+	dir := "." // get from CLI args or a list from standard input
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	addHint := true
+	// End of hardcoded things I need to turn into variables
+
 	var err error
-	dir := "."
 	files, err := os.ReadDir(dir)
 	if err != nil {
-		panic(fmt.Sprintf("error reading directory %s: %s\n", dir, err))
+		return fmt.Errorf("error reading directory %s: %s", dir, err)
 	}
+
 	entries := make(Entries, len(files))
 	for pos, file := range files {
 		line := pos + 1
@@ -61,71 +130,68 @@ func main() {
 		entries[line] = e
 	}
 
-	//	tmpFile, err := os.CreateTemp("", "editdir.tmp")
+	// Create temporary file to be edited
+	editableList := entries.String()
+	tmpFile, err := os.CreateTemp("", "editdir.")
 	if err != nil {
-		panic(fmt.Sprintf("error creating temporary file: %s\n", err))
+		return fmt.Errorf("Error creating temporary file for edition: %s", err)
+	}
+	defer os.Remove(tmpFile.Name())
+
+	if addHint {
+		tmpFile.WriteString("# Lines starting with # or empty are ignored.\n")
+		tmpFile.WriteString("# Lines starting with 'cancel' or 'abort' tell the program to do nothing on exit. As well as exiting the editor with non-zero return code.\n")
+		tmpFile.WriteString("# Valid path lines have the format 'ID<space>NewPath'. They must start with a number (no spaces before) followed by a space and the new path for that file.\n")
+		tmpFile.WriteString("# The number is the ID referencing to the original list so you can reorder the lines as long as the ID is kept.\n")
+		tmpFile.WriteString("# Deleted lines tell the program that it must delete that file from disk.\n")
+	}
+	tmpFile.WriteString(editableList)
+	log.Debug().Str("tmpFile", tmpFile.Name()).Msg("Temporary file created for edition")
+
+	// Run the editor
+	editorBin := "vi"
+	editorArgs := []string{tmpFile.Name()}
+	cmd := exec.Command(editorBin, editorArgs...)
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	err = cmd.Run()
+	if err != nil {
+		return fmt.Errorf("Canceling due to no clean execution and exit of editor: Exit Message=%s (%T), Return Code=%d\n", err, err, cmd.ProcessState.ExitCode())
 	}
 
-	output, err := BuildEditList(entries)
+	// Load edited entries
+	tmpContent, err := os.ReadFile(tmpFile.Name())
 	if err != nil {
-		panic(err)
+		return fmt.Errorf("Error reading file with edited paths: %v", err)
 	}
 
-	scanner := bufio.NewScanner(strings.NewReader(output))
-	for scanner.Scan() {
-		entry := ParseLine(scanner.Text())
-		switch entry.Status {
-		case StatusAbort:
-			fmt.Println("Aborting execution without modifying any files")
-			return
-		case StatusIgnore:
-			continue
-		case StatusRename:
-			e := entries[entry.ID]
-			e.NewPath = entry.Path
-			entries[entry.ID] = e
-			fmt.Println(e)
-		default:
-			panic(fmt.Sprintf("Invalid status: %d\n", entry.Status))
+	editedEntries, err := LoadEditedList(string(tmpContent))
+	if err != nil {
+		// If it was cancelled does not tell it's an error
+		for _, e := range editedEntries {
+			if e.Status == StatusCancel {
+				return e.Error // contains the line in which the cancelation was present
+			}
 		}
+		return fmt.Errorf("Error parsing the edited paths: %v", err)
 	}
 
-	// Load CLI options
-	// Receive the path where dirs reside and store in a temporary file
-	// add line numbers to each path in the temporary file
-	// add to the temp file a line showing how this works
-	// Run the editor on that temporary file and wait for it to finish
-	// when the editor exists check its return code, if rc!= exit
-	// read all lines of the temp file, if any of them have the word cancel or abort then exit
-	// check if there are no lines with the same number, exit with error
-	// check if there are are new files with the same name (duplicates). Warn
+	// Merge edited paths
+	entries.Update(editedEntries)
+	for _, e := range entries {
+		fmt.Println(e)
+	}
 
-	// options to:
-	// delete file if line is deleted from file
-	// enable check for duplicate names in output
-	// enable check for when the file already exists
-	// include dot files (call it 'hidden' if I can work with hidden files in windows)
-	// load files recursively, this implies: needs to know what to do with directories, what to do with directory entries, etc
+	return nil
 }
 
-func BuildEditList(entries Entries) (string, error) {
-	var output strings.Builder
-	keys := sort.IntSlice{}
-	for k, _ := range entries {
-		keys = append(keys, k)
-	}
-	keys.Sort()
-	for _, key := range keys {
-		e := entries[key]
-		_, err := output.WriteString(fmt.Sprintf("%d %s\n", e.ID, e.Path))
-		if err != nil {
-			return "", fmt.Errorf("error building edit list: %v", err)
-		}
-	}
-	return output.String(), nil
-}
-
-func ParseLine(line string) Entry {
+// ParseEditedLine will read a line string and try to extract an Entry from it.
+// It expects the line to start with and ID and a Path
+// The ID will reference the entry in the input line and will be used to match
+// the old name with the new/edited name for the program to rename or delete
+// that file
+func ParseEditedLine(line string) Entry {
 	var entry Entry
 	// TODO: Trim other kinds of unicode space
 	trimmed := strings.Trim(line, " \r\n")
@@ -141,7 +207,7 @@ func ParseLine(line string) Entry {
 
 	if strings.HasPrefix(strings.ToLower(trimmed), "cancel") ||
 		strings.HasPrefix(strings.ToLower(trimmed), "abort") {
-		entry.Status = StatusAbort
+		entry.Status = StatusCancel
 		return entry
 	}
 	parts := strings.SplitN(line, " ", 2)
@@ -156,4 +222,54 @@ func ParseLine(line string) Entry {
 	entry.Path = strings.Trim(parts[1], " \r\n")
 	entry.Status = StatusRename
 	return entry
+}
+
+// LoadEditedList receives a string containing multiple lines, each line with
+// a possible Entry in text form (before being parsed into and Entry).
+// It returns Entries, error and/or indication of cancellation
+func LoadEditedList(text string) (Entries, error) {
+	editedEntries := Entries{}
+	scanner := bufio.NewScanner(strings.NewReader(text))
+	lineNum := 0
+	for scanner.Scan() {
+		entry := ParseEditedLine(scanner.Text())
+		lineNum++
+		switch entry.Status {
+		case StatusCancel:
+			// Return a single entry to indicate that the program should be cancelled
+			err := fmt.Errorf("Cancelling execution without modifying any files due to cancel/abort clause in line %d", lineNum)
+			ent := Entries{}
+			ent[0] = Entry{
+				ID:     0,
+				Error:  err,
+				Status: StatusCancel,
+			}
+			return ent, err
+		case StatusIgnore:
+			log.Debug().Str("line", scanner.Text()).Msg("Ignored line")
+			continue
+		case StatusRename:
+			editedEntries[entry.ID] = entry
+		default:
+			return editedEntries, fmt.Errorf("Invalid status: %v. Error=%s\n", entry.Status, entry.Error)
+		}
+	}
+	return editedEntries, nil
+}
+
+// entriesFromLines receives a string with a file path in each line and
+// return an Entries struct with the line number as the ID and the line text
+// as Path of each entry
+func entriesFromLines(text string) Entries {
+	entries := Entries{}
+	scanner := bufio.NewScanner(strings.NewReader(text))
+	line := 1
+	for scanner.Scan() {
+		e := Entry{ID: line,
+			Path:   scanner.Text(),
+			Status: StatusIgnore}
+		entries[e.ID] = e
+		line++
+	}
+	return entries
 }
--- main_test.go
+++ main_test.go
@@ -31,7 +31,7 @@ func TestParseLineNonRenameStatus(t *testing.T) {
 func TestParseLineTrimmed(t *testing.T) {
 	e := ParseLine("2   file   a.txt   ")
 	if e.Status != StatusRename {
-		t.Fatalf("Entry should be StatusRename, got: %d ", e.Status)
+		t.Fatalf("Entry should be StatusRename, got: %v ", e.Status)
 	}
 	if e.Error != nil {
 		t.Fatalf("Entry should not have an error: %s ", e.Error)
@@ -47,7 +47,7 @@ func TestParseLineTrimmed(t *testing.T) {
 func TestParseLineUnixNewLineEnd(t *testing.T) {
 	e := ParseLine("2 file   a.txt\n")
 	if e.Status != StatusRename {
-		t.Fatalf("Entry should be StatusRename, got: %d ", e.Status)
+		t.Fatalf("Entry should be StatusRename, got: %v", e.Status)
 	}
 	if e.Error != nil {
 		t.Fatalf("Entry should not have an error: %s ", e.Error)
@@ -63,7 +63,7 @@ func TestParseLineUnixNewLineEnd(t *testing.T) {
 func TestParseLineWindowsNewLineEnd(t *testing.T) {
 	e := ParseLine("2 file   a.txt\r\n")
 	if e.Status != StatusRename {
-		t.Fatalf("Entry should be StatusRename, got: %d ", e.Status)
+		t.Fatalf("Entry should be StatusRename, got: %v", e.Status)
 	}
 	if e.Error != nil {
 		t.Fatalf("Entry should not have an error: %s ", e.Error)
@@ -73,6 +73,106 @@ func TestParseLineWindowsNewLineEnd(t *testing.T) {
 	}
 	if e.Path != "file   a.txt" {
 		t.Fatalf("Entry's path is not 'file   a.txt', it is %q", e.Path)
+	}
+}
+
+func TestEntriesFromLines(t *testing.T) {
+	es := entriesFromLines(`file1.txt
+file 2.txt
+`)
+	if len(es) != 2 {
+		t.Fatalf("Invalid number of entries from lines: %d!=2", len(es))
+	}
+	e1, ok := es[1]
+	if !ok {
+		t.Fatalf("Should have found ID 1")
+	}
+	if e1.ID != 1 {
+		t.Fatalf("Entry 1 should have ID 1")
+	}
+	if e1.Path != "file1.txt" {
+		t.Fatalf("Entry 1 has bad Path: %v", e1.Path)
+	}
+
+	e2, ok := es[2]
+	if !ok {
+		t.Fatalf("Should have found ID 2")
+	}
+	if e2.ID != 2 {
+		t.Fatalf("Entry 2 should have ID 2")
+	}
+	if e2.Path != "file 2.txt" {
+		t.Fatalf("Entry 2 has bad Path: %v", e2.Path)
+	}
+
+}
+
+func TestEditedList(t *testing.T) {
+	entries := entriesFromLines(`file1.txt
+file 2.txt
+file 3.txt
+file 4.txt
+`)
+	edited, err := LoadEditedList(`1 file1.txt
+2 file TWO.txt
+4 file 4.txt
+`)
+	if err != nil {
+		t.Fatalf("Error preparing edited list in this test")
+	}
+
+	entries.Update(edited)
+	e1, ok := entries[1]
+	if !ok {
+		t.Fatalf("Should have found ID 1")
+	}
+	if e1.Path != "file1.txt" {
+		t.Fatalf("Entry 1 has bad Path: %v", e1.Path)
+	}
+	if e1.Status != StatusRename {
+		t.Fatalf("Entry 1 should have StatusRename: %v != %v", StatusRename, e1.Status)
+	}
+	if e1.NewPath == "" {
+		t.Fatalf("Entry 1 should have a NewPath")
+	}
+
+	e2, ok := entries[2]
+	if !ok {
+		t.Fatalf("Should have found ID 2")
+	}
+	if e2.Path != "file 2.txt" {
+		t.Fatalf("Entry 2 has bad Path: %v", e2.Path)
+	}
+	if e2.Status != StatusRename {
+		t.Fatalf("Entry 2 should have StatusRename: %v != %v", StatusRename, e2.Status)
+	}
+	if e2.NewPath == "" {
+		t.Fatalf("Entry 2 should have a NewPath")
+	}
+
+	e3, ok := entries[3]
+	if !ok {
+		t.Fatalf("Should have found ID 3")
+	}
+	if e3.ID != 3 {
+		t.Fatalf("Entry 3 should have ID 3, has %d", e3.ID)
+	}
+	if e3.Status != StatusDelete {
+		t.Fatalf("Entry 3 should have StatusDelete: %v != %v", StatusDelete, e3.Status)
+	}
+
+	e4, ok := entries[4]
+	if !ok {
+		t.Fatalf("Should have found ID 4")
+	}
+	if e4.Path != "file 4.txt" {
+		t.Fatalf("Entry 4 has bad Path: %v", e4.Path)
+	}
+	if e4.Status != StatusRename {
+		t.Fatalf("Entry 4 should have StatusRename: %v != %v", StatusRename, e4.Status)
+	}
+	if e4.NewPath == "" {
+		t.Fatalf("Entry 4 should have a NewPath")
 	}
 }
 
